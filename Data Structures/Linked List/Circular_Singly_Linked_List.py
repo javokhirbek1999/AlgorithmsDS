30d42241aa22b0bb6b9a7285ab3ce937a16b5985
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-03-21 19:01:59 +0100

    Update Circular Singly Linked List

--- Data Structures/Linked List/Circular_Singly_Linked_List.py
+++ Data Structures/Linked List/Circular_Singly_Linked_List.py
@@ -16,92 +16,43 @@ class CircularSinglyLinkedList:
             if node.next == self.head:
                 break
             else:
-                node = node.next
-
-    # Creates Circular Singly Linked List
-    def createCSLL(self, value):
-        node = Node(value)
-        node.next = node
-        self.head = node
-        self.tail = node
-        self.size += 1   
-
+                node = node.next 
+    
     def prepend(self, value):
+        node = Node(value)
         if self.head is None:
-            return None
-        else:    
-            node = Node(value)
+            self.head = node
+            self.tail = node
+            self.tail.next = self.head
+        else:
             node.next = self.head
             self.head = node
-            self.tail.next = self.head 
-            self.size += 1
-
+            self.tail.next = self.head
+        self.size += 1    
+    
     def append(self, value):
+        node = Node(value)
         if self.head is None:
-            return None
+            self.head = node
+            self.tail = node
+            self.tail.next = self.head
         else:
-            node = Node(value)
             self.tail.next = node
             self.tail = node
             self.tail.next = self.head
-            self.size += 1
-
+        self.size += 1
+    
     def insert(self, value, index):
-        new_node = Node(value)
+        node = Node(value)
         if self.head is None:
-            return None
+            self.head = node
+            self.tail = node
+            self.tail.next = self.head
         else:
             if index == 0:
                 self.prepend(value)
             elif index == self.size-1:
                 self.append(value)
-            else:
-                node = self.head
-                ind = 0
-                while ind < index - 1:
-                    node = node.next
-                    ind += 1
-                next_node = node.next
-                node.next = new_node
-                new_node.next = next_node 
-
-    def remove(self, value):
-        if self.head is None:
-            return None
-        else:
-            current = self.head
-            prev = self.head
-        
-            while current:
-                if current.value == value:
-                    if current == self.head:
-                        self.head = self.head.next
-                        self.tail.next = self.head
-                    else:
-                        prev.next = current.next
-                    self.size -= 1
-                    return 
-                prev = current
-                current = current.next
-    
-    def remove_by_index(self, index):
-        if self.head is None:
-            return None
-        else:
-            if index == 0:
-                self.head = self.head.next
-                self.tail.next = self.head
-                self.size -= 1
-            elif index == self.size-1:
-                current = self.head
-                while current:
-                    if current.next == self.tail:
-                        break
-                    else: 
-                        current = current.next    
-                self.tail = current
-                self.tail.next = self.head
-                self.size -= 1 
             else:
                 current = self.head
                 ind = 0
@@ -109,22 +60,73 @@ class CircularSinglyLinkedList:
                     current = current.next
                     ind += 1
                 next_node = current.next
-                current.next = next_node.next    
+                current.next = node
+                node.next = next_node
+                self.size += 1
+    
+    def remove(self, value):
+        if self.head is None:
+            return None
+        else:
+            current = self.head
+            prev = self.head
+            while current:
+                if current.value == value:
+                    if current == self.tail:
+                        self.head = None
+                        self.tail = None
+                    else:
+                        prev.next = current.next
+                    self.size -= 1
+                    return
+                else:
+                    prev = current
+                    current = current.next 
+    
+    def remove_by_index(self, index):
+        if self.head is None:
+            return None
+        else:
+            if index == 0:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+                    self.tail.next = self.head
                 self.size -= 1
-        
-
+            elif index == self.size-1:
+                current = self.head
+                while current:
+                    if current.next == self.tail:
+                        break
+                    else:
+                        current = current.next
+                self.tail = current
+                self.tail.next = self.head
+                self.size -= 1 
+            else:
+                current = self.head
+                ind = 0
+                while ind < index-1:
+                    current = current.next
+                    ind += 1
+                next_node = current.next
+                current.next = next_node.next
+                self.size -= 1
+    
     def traverse(self):
         if self.head is None:
             return None
-        else:    
+        else:
             current = self.head
             while current:
                 if current.next == self.head:
                     break
                 else:
                     print(current.value)
-                    current = current.next   
-
+                    current = current.next
+    
     def search(self, value):
         if self.head is None:
             return None
@@ -137,11 +139,11 @@ class CircularSinglyLinkedList:
                     if current.value == value:
                         return True
                     else:
-                        current = current.next
-            return False 
+                        current = current.next 
+            return False
     
     def clear(self):
         self.head = None
         self.tail.next = None
-        self.tail = None 
+        self.tail = None
         self.size = 0
