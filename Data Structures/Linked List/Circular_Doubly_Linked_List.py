65dfed788f452f44aa7fd4ce50a39ac447624d7b
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-03-25 18:30:44 +0100

    Update Circular Doubly Linked List

--- Data Structures/Linked List/Circular_Doubly_Linked_List.py
+++ Data Structures/Linked List/Circular_Doubly_Linked_List.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, value=None):
+    def __init__(self,value):
         self.value = value
         self.next = None
         self.prev = None
@@ -11,52 +11,51 @@ class CircularDoublyLinkedList:
         self.size = 0
 
     def __iter__(self):
-        current = self.head
-        while current:
-            yield current
-            current = current.next
-            if current == self.tail.next:
+        node = self.head
+        while node:
+            yield node
+            if node.next == self.head:
                 break
+            else:
+                node = node.next     
+
     def prepend(self, value):
         node = Node(value)
         if self.head is None:
-            node.prev = node
             node.next = node
+            node.prev = node
             self.head = node
             self.tail = node
-            self.size += 1     
         else:
-            node.prev = self.tail
             node.next = self.head
             self.head = node
-            self.tail.next = self.head    
-            self.size += 1     
-             
-
-    def append(self, value):
+            self.head.prev = self.tail
+            self.tail.next = self.head 
+        self.size += 1
+    
+    def append(self,value):
         node = Node(value)
         if self.head is None:
-            node.prev = node
             node.next = node
+            node.prev = node
             self.head = node
             self.tail = node
-            self.size += 1     
-        else:           
+        else:
             self.tail.next = node
+            node.prev = self.tail
             self.tail = node
-            self.tail.next = self.head   
-            self.size += 1     
-
-    def insert(self, value, index):
+            self.tail.next = self.head
+            self.head.prev = self.tail
+        self.size += 1
+    
+    def insert(self,value,index):
         node = Node(value)
         if self.head is None:
-            return Exception('List Does not exist')
+            raise Exception('List does not exist')
         else:
-            if index > self.size:
-                raise Exception('Invalid index')
             if index == 0:
                 self.prepend(value)
-            elif index == self.size:
+            elif index == self.size-1 or index==-1:
                 self.append(value)
             else:
                 current = self.head
@@ -64,62 +63,47 @@ class CircularDoublyLinkedList:
                 while ind < index-1:
                     current = current.next
                     ind += 1
-                node.next = current.next   
-                node.prev = current 
-                current.next = node    
-                self.size += 1 
+                node.next = current.next
+                node.prev = current
+                current.next = node
+                self.size += 1
     
-    def remove(self, value):
+    def remove(self,value):
         if self.head is None:
             raise Exception('List does not exist')
         else:
             current = self.head
-            prev = self.head
             while current:
                 if current.value == value:
                     if current == self.head:
                         self.head = self.head.next
-                        self.head.prev = self.tail 
-                        
+                        self.head.prev = self.tail
+                        self.tail.next = self.head
                     elif current == self.tail:
                         self.tail = self.tail.prev
                         self.tail.next = self.head
                         self.head.prev = self.tail
                     else:
-                        prev.next = current.next
+                        current.prev.next = current.next
+                        current.next.prev = current.prev
                     self.size -= 1
-                    return 
-                else:
-                    prev = current
-                    current = current.next                      
-     
+                    return
+                current = current.next        
 
-    def remove_by_index(self, index):
+    def remove_by_index(self,index):
         if self.head is None:
             raise Exception('List does not exist')
-        elif index > self.size:
-            raise Exception('Invalid index')    
+        if index > self.size:
+            raise Exception('Invalid Index')    
         else:
             if index == 0:
-                if self.head == self.tail:
-                    self.head.prev = None
-                    self.head.next = None
-                    self.head = None
-                    self.tail = None
-                else:    
-                    self.head = self.head.next
-                    self.head.prev = self.tail
-                    self.tail.next = self.head
-            elif index == self.size:
-                if self.head == self.tail:
-                    self.head.prev = None
-                    self.head.next = None
-                    self.head = None
-                    self.tail = None
-                else:
-                    self.tail = self.tail.prev
-                    self.tail.next = self.head
-                    self.head.prev = self.tail
+                self.head = self.head.next
+                self.head.prev = self.tail
+                self.tail.next = self.head
+            elif index == self.size-1:
+                self.tail = self.tail.prev
+                self.tail.next = self.head
+                self.head.prev = self.tail
             else:
                 current = self.head
                 ind = 0
@@ -127,35 +111,10 @@ class CircularDoublyLinkedList:
                     current = current.next
                     ind += 1
                 current.next = current.next.next
-                current.next.prev = current
-            self.size -= 1
+                current.next.prev = current    
+                self.size -=1 
 
-    def traverse(self):
-        if self.head is None:
-            raise Exception('List does not exist')
-        else:
-            current = self.head
-            while current:
-                if current.next == self.head:
-                    break
-                else:
-                    print(current.value)
-                current = current.next     
-
-    def contains(self, value):
-        if self.head is None:
-            raise Exception('List does not exist')
-        else:
-            current = self.head
-            while current:      
-                if current.value == value:
-                    return True
-                if current.next == self.head:
-                    break    
-                current = current.next
-            return False           
-
-    def search(self, value):
+    def search(self,value):
         if self.head is None:
             raise Exception('List does not exist')
         else:
@@ -168,12 +127,37 @@ class CircularDoublyLinkedList:
                     break
                 else:
                     current = current.next
-                    ind += 1   
+                    ind += 1
 
+    def contains(self,value):
+        if self.head is None:
+            raise Exception('List does not exist')
+        else:
+            current = self.head
+            while current:
+                if current.value == value:
+                    return True
+                current = current.next
+                if current.next == self.head:
+                    break
+            return False       
+
+    def traverse(self):
+        if self.head is None:
+            raise Exception('List does not exist')
+        else:
+            current = self.head
+            while current:
+                if current.next == self.head:
+                    break
+                print(current.value)
+                current = current.next  
+              
+            
     def clear(self):
         if self.head is None:
             raise Exception('List does not exist')
-        else:    
+        else:
             self.head = None
-            self.tail = None   
-            self.size = 0    
+            self.tail = None
+            self.size = 0      
