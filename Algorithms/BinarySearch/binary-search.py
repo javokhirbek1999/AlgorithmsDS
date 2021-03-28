27d15f0d2d97eda8947a39ebc865da615b8f97ae
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-03-28 03:08:43 +0200

    Create binary-search.py

---
+++ Algorithms/BinarySearch/binary-search.py
@@ -0,0 +1,14 @@
+def binary_search(arr,item):
+    low = arr[0]
+    high = arr[-1]
+
+    while low<=high:
+        mid = (low+high)//2
+        guess = arr[mid]
+        if guess == item:
+            return mid
+        if guess > arr[mid]:
+            low = mid+1
+        else:
+            high = mid-1
+        return -1    
