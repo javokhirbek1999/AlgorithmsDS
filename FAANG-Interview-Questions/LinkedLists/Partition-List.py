cadd9e191112a57a32a9ce0230220e82042c2c89
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-04-02 05:45:39 +0200

    Create Partition-List.py

---
+++ FAANG-Interview-Questions/LinkedLists/Partition-List.py
@@ -0,0 +1,23 @@
+from LinkedList import LinkedList
+
+def partition(ListNode,x):
+    if ListNode.head is None:
+        return None
+
+    current = ListNode.head
+    ListNode.tail = ListNode.head
+    
+    while current:
+        next_node = current.next
+        current.next = None
+        if current.value < x:
+            current.next = ListNode.head
+            ListNode.head = current
+        else:
+            ListNode.tail.next = current
+            ListNode.tail = current
+        current = next_node
+       
+    if ListNode.tail.next is not None:
+        ListNode.tail.next = None
+    return ListNode
