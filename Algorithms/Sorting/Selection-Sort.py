a68cd5d7c511877b4905f382c5c31cbcbedab06e
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-05-01 06:34:43 +0200

    Update Selection-Sort.py

--- Algorithms/Sorting/Selection-Sort.py
+++ Algorithms/Sorting/Selection-Sort.py
@@ -1,27 +1,18 @@
-# Time: O(n) 
-def find_smallest(arr):
-    smallest = arr[0]
-    smallest_index = 0
-    for i in range(1,len(arr)): # <- O(n)
-        if arr[i]<smallest:
-            smallest = arr[i]
-            smallest_index = i
-    return smallest_index
+# Time: O(n^2) -> Nested loops: one for looping through the array the min val and two for finding the min val
+# Space: O(1) -> Sorts by swaping in-place without requiring additional space 
+def selectionSort(arr):
+    
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i+1,len(arr)): # Find min value index
+            if arr[min_index] > arr[j]:
+                min_index = j
+        arr[i],arr[min_index] = arr[min_index],arr[i] # Sort by swaping the values in-place: O(1)
+    return arr
 
-def selection_sort(arr):
-    new_arr = []
-    for i in range(len(arr)): # <-- O(n)
-        smallest = find_smallest(arr) # <-- O(n)
-        new_arr.append(arr.pop(smallest))
-    return new_arr
-# Time: O(n x n) = O(n^2)
+# When to use Selection Sort:
+# 1. When space is the main concern
+# 2. Easy to implement
 
-# Selection sort costs O(n^2) as it has two nested loops: 
-# 1. First loop for finding the smallest number in the array
-# 2. For looping through the array to append the elements in order from smallest to greatest
-
-# Algorithm:
-# 1. Create a new array to store the sorted elements 
-# 2. Find the smallest value in the array: Helper function for this case would be helpful  
-# 3. Add that smallest value to the new array and remove it from the array immediately: pop() is the best option for this case
-# 4. Keep repeating step 2 and 3 until the original array is empty
+# When to avoid to use Selection Sort:
+# 1. When time is a concer 
