8bcf4fc2f63de245365040c90bb08771d0bb1a20
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-07-06 19:16:58 +0500

    Disjoint Set (Union Find) Implementation

---
+++ Algorithms/Graphs/DisjoinSet(Union-Find).py
@@ -0,0 +1,24 @@
+class DisjointSet:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.parent = {}
+        for v in vertices:
+            self.parent[v] = v
+        self.rank = dict.fromkeys(vertices, 0)
+    
+    def find(self, item):
+        if self.parent[item] == item:
+            return item
+        else:
+            return self.find(self.parent[item])
+    
+    def union(self, x, y):
+        xroot = self.find(x)
+        yroot = self.find(y)
+        if self.rank[xroot] < self.rank[yroot]:
+            self.parent[xroot] = yroot
+        elif self.rank[xroot] > self.rank[yroot]:
+            self.parent[yroot] = xroot
+        else:
+            self.parent[xroot] = yroot
+            self.rank[xroot] += 1
