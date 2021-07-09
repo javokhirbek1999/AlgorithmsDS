94e95b19f24ab4137e18dd43ff05503db359b82c
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-07-10 04:40:12 +0500

    Create CoinChange.py

---
+++ Algorithms/Greedy/CoinChange.py
@@ -0,0 +1,15 @@
+def coinChange(totalAmount, coins):
+	n = totalAmount
+	index = len(coins)-1
+	min_coin_set = []
+	while True:
+		coinValue = coins[index]
+		if n >= coinValue:
+			min_coin_set.append(coinValue)
+			n = n - coinValue
+
+		if n < coinValue:
+			index-=1
+
+		if n==0:
+			return min_coin_set
