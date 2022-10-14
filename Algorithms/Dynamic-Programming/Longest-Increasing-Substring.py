0744aa13e96c5084ec18d8fded7cfaf464a09596
Javokhirbek Khaydaraliev <54243963+javokhirbek1999@users.noreply.github.com>
2022-10-14 17:50:17 +0200

    Create Longest-Increasing-Substring.py

---
+++ Algorithms/Dynamic-Programming/Longest-Increasing-Substring.py
@@ -0,0 +1,23 @@
+"""
+Time: O(n^2)
+Space: O(n)
+"""
+
+def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        res = []
+        
+        n = len(nums)
+        
+        for _ in range(n):
+            res.append(1)
+        
+        maxLen = 0
+
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] < nums[j] and res[i] >= res[j]:
+                    res[j] += 1
+                    maxLen = max(maxLen, res[j])  
+
+        return maxLen
