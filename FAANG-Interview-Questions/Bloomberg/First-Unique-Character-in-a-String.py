75d9abe648bee540f205c217c0e007708255b83a
Javokhirbek Khaydaraliev <54243963+javokhirbek1999@users.noreply.github.com>
2022-10-18 12:46:24 +0200

    Update First-Unique-Character-in-a-String.py

--- FAANG-Interview-Questions/Bloomberg/First-Unique-Character-in-a-String.py
+++ FAANG-Interview-Questions/Bloomberg/First-Unique-Character-in-a-String.py
@@ -1,5 +1,38 @@
 """
 Time: O(n)
+Space: O(1)
+"""
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        
+        if len(s) == 1:
+            return 0
+        
+        charFreq = {char:[0,math.inf] for char in "abcdefghijklmnopqrstuvwxyz"}
+        
+        curr = 1
+        for char in s:
+            charFreq[char][0] += 1
+            
+            if charFreq[char][1] == math.inf:
+                charFreq[char][1] = curr
+                curr += 1
+        
+        
+        _min = math.inf
+        ind = -1
+        for index in range(len(s)):
+            if charFreq[s[index]][0] == 1:
+                if charFreq[s[index]][1] < _min:
+                    _min = charFreq[s[index]][1]
+                    ind = index
+                
+        return ind
+    
+
+
+"""
+Time: O(n)
 Space: O(n)
 """
 from collections import Counter
