e15b16057cfe8986525c12b4040b988a0f795458
Javokhirbek Khaydaraliev <54243963+javokhirbek1999@users.noreply.github.com>
2022-10-25 09:47:29 +0200

    Update Visiting-Cities.py

--- FAANG-Interview-Questions/Citadel/Visiting-Cities.py
+++ FAANG-Interview-Questions/Citadel/Visiting-Cities.py
@@ -2,23 +2,21 @@
 Time: O(n)
 Space: O(n)
 """
-def solve(red: List[int], blue: List[int], blueCost: int) -> int:
+def visitingAllCities(red: List[int], blue: List[int], blueCost: int) -> int:
 
     n = len(red)
 
     dp = [0]
 
-    prevBlueLine = -1
+    onBlueLine = False
 
     for i in range(n):
-        takeRed = dp[-1] + red[i]
-        takeBlue = dp[-1] + blue[i] + blueCost if prevBlueLine != blue[i] else dp[-1] + blue[i]
-        
-        prevBlueLine = blue[i]
-        
-        curr = min(takeRed, takeBlue)
 
-        dp.append(curr)
-        
+        takeRed = red[i]
+        takeBlue = blue[i] + blueCost if not onBlueLine else blue[i]
 
-    return dp
+        onBlueLine = True if takeBlue <= takeRed else False
+
+        dp.append(dp[-1] + min(takeRed, takeBlue))
+
+    return dp 
