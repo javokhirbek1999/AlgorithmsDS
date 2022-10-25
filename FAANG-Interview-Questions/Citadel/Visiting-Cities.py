3ba71a485dfc04231762a6141b6fdb0ff97dbf4e
Javokhirbek Khaydaraliev <54243963+javokhirbek1999@users.noreply.github.com>
2022-10-25 09:49:15 +0200

    Update Visiting-Cities.py

--- FAANG-Interview-Questions/Citadel/Visiting-Cities.py
+++ FAANG-Interview-Questions/Citadel/Visiting-Cities.py
@@ -20,3 +20,19 @@ def visitingAllCities(red: List[int], blue: List[int], blueCost: int) -> int:
         dp.append(dp[-1] + min(takeRed, takeBlue))
 
     return dp 
+
+
+"""
+Alternative
+
+Time: O(n)
+Space: O(n)
+"""
+
+def solve(red, blue, blueCost):
+    ans=[0]
+    r,b=0,blueCost 
+    for i,j in zip(red,blue):
+        r,b=min(r,b)+i,min(b,r+blueCost)+j
+        ans+=[min(r,b)]  
+    return ans 
