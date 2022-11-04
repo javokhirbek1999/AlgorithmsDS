aed4ddea993d24a1d71b855555f001188caba9e9
Javokhirbek Khaydaraliev <54243963+javokhirbek1999@users.noreply.github.com>
2022-11-04 08:31:18 +0100

    Create Word-Break.py

---
+++ Algorithms/Dynamic-Programming/String/Word-Break.py
@@ -0,0 +1,25 @@
+"""
+Time: O(n^2)
+Space: O(n)
+"""
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        n = len(s)
+        
+        dp = [False] * (n+1)
+        
+        dp[0] = True
+        
+        wDict = {word:1 for word in wordDict}
+        
+        
+        for i in range(n+1):
+            
+            for j in range(i):
+                
+                if dp[j] and s[j:i] in wDict:
+                    dp[i] = True
+                    break
+        
+        return dp[n]
