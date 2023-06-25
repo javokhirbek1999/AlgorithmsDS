9cf15d3e706d84aa1914c89f831b5d214cb12708
Javokhirbek Khaydaraliev <54243963+javokhirbek1999@users.noreply.github.com>
2023-06-25 10:51:48 +0200

    Create Divisors.py

---
+++ Competitive-Programming/Number-Theory/Divisors.py
@@ -0,0 +1,16 @@
+"""
+TIP: model is as a equation
+
+1. A | B => A divides B => B is divisible by A => B % A == 0
+
+2. Divisors (or factors) of X: a number Y where Y | X
+
+3. Transitive: A|B, B|C => A|C
+
+4. Prime: no divisor expect itself
+
+5. Prime factorization: Breaking a number down into a product of primes (or prime powers: P^K for prime P and integer K)
+
+    Ex: 2 = 2, 10 = 2 * 5, 254436 = 2^3 * 3 * 7 * 13 * 233
+        27 = 3 * 9 not valid, 9 can be further broken down, 27 = 3^3
+"""
